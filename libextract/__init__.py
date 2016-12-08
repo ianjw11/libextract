2f4e0a868687da7511713f384cb5e1b4e29d5257
Ian Weisberger <ian@factionworks.com>
2016-12-08 04:39:23 -0500

    added Extractor

---
+++ libextract/__init__.py
@@ -0,0 +1,1 @@
+print "imported libextract from filesystem"
--- libextract/api.py
+++ libextract/api.py
@@ -13,3 +13,24 @@ def extract(document, encoding='utf-8', count=None):
         parse_html(document, encoding=encoding),
         (select, measure, crank, finalise)
         )
+
+
+class Extractor(object):
+
+  def extract_lxml(self, elem, count=None):
+    crank = partial(rank, count=count) if count else rank
+    return pipeline(
+        elem,
+        (select, measure, crank, finalise))
+
+  def extract_response(self, response, count=None):
+    self.elem = elem = response.xpath('.')[0].root
+    return self.extract_lxml(elem, count)
+
+  def extract(self, document, encoding='utf-8', count=None):
+      if isinstance(document, bytes):
+          document = BytesIO(document)
+      self.elem = elem = parse_html(document, encoding=encoding)
+
+      return self.extract_lxml(elem, count)
+
